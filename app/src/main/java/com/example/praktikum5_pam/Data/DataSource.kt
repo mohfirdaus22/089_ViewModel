8fa09a636de5628f6799f35ba8fe237742cef692
mohfirdaus22 <thepashter6@gmail.com>
2023-10-20 14:24:33 +0700

    Add String deskripsi dan datasource

---
+++ app/src/main/java/com/example/praktikum5_pam/Data/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum5_pam.Data
+
+import com.example.praktikum5_pam.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+
+}
