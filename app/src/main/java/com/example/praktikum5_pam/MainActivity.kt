504788f39f3534dd19cfb766cd3fc2cf9c7a6f25
mohfirdaus22 <thepashter6@gmail.com>
2023-10-20 15:26:34 +0700

    Add fun Select (RadioButton)

--- app/src/main/java/com/example/praktikum5_pam/MainActivity.kt
+++ app/src/main/java/com/example/praktikum5_pam/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.praktikum5_pam
 
+import android.content.pm.ChangedPackages
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.praktikum5_pam.ui.theme.Praktikum5_PAMTheme
@@ -33,6 +44,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun SelectJK(
+    options: List<String>,
+    onSelectionChanged: (String) ->Unit ={}
+){
+    var selectedValue by rememberSaveable { mutableStateOf("") }
+
+    Column( modifier = Modifier.padding(16.dp)) {
+        options.forEach{ item ->
+            Row (
+                modifier = Modifier.selectable(
+                    selected = selectedValue == item,
+                    onClick = {
+                        selectedValue = item
+                        onSelectionChanged(item)
+                    }
+                ),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                RadioButton(
+                    selected = selectedValue == item,
+                    onClick = {
+                        selectedValue = item
+                        onSelectionChanged(item)
+                    }
+                )
+                Text(item)
+            }
+        }
+    }
+}
+
+
 
 @Composable
 fun TextHasil(namanya : String, telponnya: String, jenisnnya: String){
