8a1ac396c2862a5bbadb7258cdf38d5f5424e7f6
mohfirdaus22 <thepashter6@gmail.com>
2023-10-20 15:55:34 +0700

    Add TampilForm

--- app/src/main/java/com/example/praktikum5_pam/MainActivity.kt
+++ app/src/main/java/com/example/praktikum5_pam/MainActivity.kt
@@ -10,22 +10,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum5_pam.Data.DataForm
+import com.example.praktikum5_pam.Data.DataSource.jenis
 import com.example.praktikum5_pam.ui.theme.Praktikum5_PAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -77,6 +88,45 @@ fun SelectJK(
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilForm(cobaViewModel: CobaViewModel = viewModel()){
+
+    var textNama by remember { mutableStateOf("") }
+    var textTlp by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+    val dataForm: DataForm
+    val uiState by cobaViewModel.uiState.collectAsState()
+    dataForm = uiState;
+
+
+    OutlinedTextField(
+        value = textNama,
+        shape = MaterialTheme.shapes.large ,
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = "Nama Lengkap")},
+        onValueChange ={
+            textNama = it
+        }
+    )
+    OutlinedTextField(
+        value = textTlp,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        shape = MaterialTheme.shapes.large ,
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = "Telpon")},
+        onValueChange ={
+            textTlp= it
+        }
+    )
+    SelectJK(
+        options = jenis.map{id ->context.resources.getString(id)},
+        onSelectionChanged = {cobaViewModel.setJenisK(it)})
+
+
+}
 
 
 @Composable
